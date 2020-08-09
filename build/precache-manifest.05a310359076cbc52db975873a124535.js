50ba6216a6281ac93ef88ae467b203e5a20cfbdf
Lucas <lcamilo@funcionalcorp.com.br>
2020-08-09 00:41:38 -0300

    Committing a build

---
+++ build/precache-manifest.05a310359076cbc52db975873a124535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52bd94aab0bead9082c1a21cdfd8a15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1533a008e7afee69cff1",
+    "url": "/static/css/main.b67ed6c6.chunk.css"
+  },
+  {
+    "revision": "89dc06f1bc652cdbf2e6",
+    "url": "/static/js/2.ae098b91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae098b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1533a008e7afee69cff1",
+    "url": "/static/js/main.10577596.chunk.js"
+  },
+  {
+    "revision": "441cfea73f775386a3f5",
+    "url": "/static/js/runtime-main.02030f09.js"
+  }
+]);
